573e75b96cc06f693ee9ddcfbce6746a20952370
Gijsjan <agijsbro@gmail.com>
2019-11-27 09:59:23 +0100

    Use single repo for projects

--- link.sh
+++ link.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-projects=( achterdeschermen gekaaptebrieven utrechtpsalter vangogh )
-cwd=`pwd`
+npm link docere-projects
 
-for project in "${projects[@]}"
-do
-	:
-	npm link "docere-project-$project"
-	cd "$cwd/node_modules/docere-project-$project"	
-	npm link docere
-	cd "$cwd"
-done
+# projects=( achterdeschermen gekaaptebrieven utrechtpsalter vangogh )
+# cwd=`pwd`
+
+# for project in "${projects[@]}"
+# do
+# 	:
+# 	npm link "docere-project-$project"
+# 	cd "$cwd/node_modules/docere-project-$project"	
+# 	npm link docere
+# 	cd "$cwd"
+# done
