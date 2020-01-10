cd378e7ee689b126c96093d07dd51c7100c4045e
Gijsjan <agijsbro@gmail.com>
2020-01-10 11:44:03 +0100

    Use React Hooks on Facsimile Panel

--- src/entry/panels/facsimile.tsx
+++ src/entry/panels/facsimile.tsx
@@ -3,26 +3,94 @@ import styled from '@emotion/styled'
 
 // TODO change facsimile when user scroll past a <pb />
 
-
 const Wrapper = styled.div`
 	background: white;
 	position: sticky;
-	${(props: Props) => {
-		if (props.orientation === Orientation.Horizontal) {
-			return `
-				top: 0;
-			`
-		}
-
-		return `
-			box-shadow: 0px -16px 24px white;
-			height: 50%;
-			top: 50%;
-		`
-	}}
+	top: 0;
 	height: 100%;
 	z-index: 1;
 `
+
+function useOpenSeaddragon(): [any, any] {
+	const [OpenSeadragon, setOpenSeadragon] = React.useState([null, null] as [any, any])
+
+	React.useEffect(() => {
+		import('openseadragon')
+			.then(OpenSeadragon => {
+				const osdInstance = OpenSeadragon.default({
+					constrainDuringPan: true,
+					controlsFadeDelay: 0,
+					controlsFadeLength: 300,
+					id: "openseadragon",
+					navigatorPosition: 'BOTTOM_LEFT',
+					// TODO only for Electron, remove before commit
+					// prefixUrl: "/home/gijs/Projects/docere/node_modules/openseadragon/build/openseadragon/images/",
+					prefixUrl: "/node_modules/openseadragon/build/openseadragon/images/",
+					sequenceMode: true,
+					showHomeControl: false,
+					showReferenceStrip: true,
+					showRotationControl: true,
+					showZoomControl: false,
+					showNavigator: true,
+					visibilityRatio: 1.0,
+				})
+				setOpenSeadragon([osdInstance, OpenSeadragon])
+			})
+	}, [])
+
+	return OpenSeadragon
+}
+
+function useHighlight(osd: any, facsimileHighlight: Props['facsimileHighlight'], OpenSeadragon: any) {
+	React.useEffect(() => {
+		if (osd == null) return
+
+		osd.removeOverlay('runtime-overlay')
+
+		var element = document.createElement("div")
+		element.id = 'runtime-overlay'
+		// element.style.boxShadow = 'rgba(255, 0, 0, 0.8) 0px 0px 4px, rgba(255, 0, 0, 0.8) 0px 0px 16px, rgba(255, 0, 0, 0.8) 0px 0px 64px'
+		element.style.border = '3px solid rgba(255, 0, 0, .6)'
+
+		// console.log(props.facsimileHighlight, new OpenSeadragon.Rect(...props.facsimileHighlight))
+		const { width: imgWidthRatio, height: imgHeightRatio } = osd.world.getHomeBounds()
+		const aspectRatio = imgHeightRatio / imgWidthRatio
+
+		// TODO check why this is necessary
+		if (facsimileHighlight == null) return
+
+		let { x, y, w, h, unit } = facsimileHighlight
+		if (unit === 'px') {
+			const { x: imgWidth, y: imgHeight } = osd.world._contentSize
+			x = x / imgWidth
+			y = y / imgHeight
+			w = w / imgWidth
+			h = h / imgHeight
+		} 
+
+		osd.addOverlay({
+			element,
+			location: new OpenSeadragon.Rect(x, y * aspectRatio, w, h * aspectRatio),
+		});
+	}, [osd, facsimileHighlight])
+}
+
+function useFacsimilePath(osd: any, activeFacsimilePath: Props['activeFacsimilePath'], projectId: Props['projectId']) {
+	React.useEffect(() => {
+		if (osd == null) return
+		// const facsimile = this.props.facsimiles.find(f => f.id === this.props.activeFacsimilePath)
+		// TODO acativeFacsimilePath should be activeFacsimileID
+		// TODO find the paths in this.props.facsimiles with activeFacsimileID
+		let path = activeFacsimilePath as any
+		
+		if (projectId === 'vangogh') {
+			path = { tileSource: { type: 'image', url: path.slice(0, -5).concat('f.png'), buildPyramid: false } }
+		}
+		
+		osd.open(path)
+	}, [osd, activeFacsimilePath])
+
+}
 
 type Props =
 	Pick<EntryState, 'activeFacsimilePath' | 'orientation'> &
@@ -30,99 +98,16 @@ type Props =
 	Pick<PanelsState, 'facsimileHighlight'> &
 	{ projectId: DocereConfig['slug'] }
 
-export default class Facsimile extends React.PureComponent<Props> {
-	private osd: any
-	private OpenSeadragon: any
+function FacsimilePanel(props: Props) {
+	const [osd, OpenSeadragon] = useOpenSeaddragon()
+	useFacsimilePath(osd, props.activeFacsimilePath, props.projectId)
+	useHighlight(osd, props.facsimileHighlight, OpenSeadragon)
 
-	componentDidMount() {
-		this.init()
-	}
-
-	componentDidUpdate(prevProps: Props) {
-		if (prevProps.activeFacsimilePath !== this.props.activeFacsimilePath) {
-			this.init()
-		}
-
-		if (prevProps.facsimileHighlight != this.props.facsimileHighlight) {
-			this.highlight()
-		}
-	}
-
-	componentWillUnmount() {
-		if (this.osd) this.osd.destroy()
-	}
-
-	render() {
-		return (
-			<Wrapper {...this.props}>
-				<div id="openseadragon" style={{ height: '100%' }}></div>
-			</Wrapper>
-		)
-	}
-
-	private async init() {
-		const osdImport = await import('openseadragon')
-		this.OpenSeadragon = osdImport.default != null ? osdImport.default : osdImport
-
-		if (this.osd == null) {
-			this.osd = this.OpenSeadragon({
-				constrainDuringPan: true,
-				controlsFadeDelay: 0,
-				controlsFadeLength: 300,
-				id: "openseadragon",
-				navigatorPosition: 'BOTTOM_LEFT',
-				// TODO only for Electron, remove before commit
-				// prefixUrl: "/home/gijs/Projects/docere/node_modules/openseadragon/build/openseadragon/images/",
-				prefixUrl: "/node_modules/openseadragon/build/openseadragon/images/",
-				sequenceMode: true,
-				showHomeControl: false,
-				showReferenceStrip: true,
-				showRotationControl: true,
-				showZoomControl: false,
-				showNavigator: true,
-				visibilityRatio: 1.0,
-			})
-		}
-
-		// const facsimile = this.props.facsimiles.find(f => f.id === this.props.activeFacsimilePath)
-		// TODO acativeFacsimilePath should be activeFacsimileID
-		// TODO find the paths in this.props.facsimiles with activeFacsimileID
-		let path = this.props.activeFacsimilePath as any
-		
-		if (this.props.projectId === 'vangogh') {
-			path = { tileSource: { type: 'image', url: path.slice(0, -5).concat('f.png'), buildPyramid: false } }
-		}
-		
-		this.osd.open(path)
-	}
-
-	private highlight() {
-		this.osd.removeOverlay('runtime-overlay')
-
-		var element = document.createElement("div")
-        element.id = 'runtime-overlay'
-		// element.style.boxShadow = 'rgba(255, 0, 0, 0.8) 0px 0px 4px, rgba(255, 0, 0, 0.8) 0px 0px 16px, rgba(255, 0, 0, 0.8) 0px 0px 64px'
-		element.style.border = '3px solid rgba(255, 0, 0, .6)'
-
-		// console.log(this.props.facsimileHighlight, new this.OpenSeadragon.Rect(...this.props.facsimileHighlight))
-		const { width: imgWidthRatio, height: imgHeightRatio } = this.osd.world.getHomeBounds()
-		const aspectRatio = imgHeightRatio / imgWidthRatio
-
-		// TODO check why this is necessary
-		if (this.props.facsimileHighlight == null) return
-
-		let { x, y, w, h, unit } = this.props.facsimileHighlight
-		if (unit === 'px') {
-			const { x: imgWidth, y: imgHeight } = this.osd.world._contentSize
-			x = x / imgWidth
-			y = y / imgHeight
-			w = w / imgWidth
-			h = h / imgHeight
-		} 
-
-        this.osd.addOverlay({
-            element,
-			location: new this.OpenSeadragon.Rect(x, y * aspectRatio, w, h * aspectRatio),
-        });
-	}
+	return (
+		<Wrapper>
+			<div id="openseadragon" style={{ height: '100%' }}></div>
+		</Wrapper>
+	)
 }
+
+export default React.memo(FacsimilePanel)
