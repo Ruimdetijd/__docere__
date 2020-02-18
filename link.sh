c1623e2b448841f437f705b4ab31a475b0015864
Gijsjan <agijsbro@gmail.com>
2020-02-18 12:36:50 +0100

    Remove docere-api

--- link.sh
+++ link.sh
@@ -5,9 +5,6 @@ cd node_modules
 rm -rf docere-projects
 ln -s ../../docere-projects docere-projects
 
-rm -rf docere-api
-ln -s ../../docere-api docere-api
-
 # rm -rf huc-faceted-search
 # ln -s ../../huc-faceted-search huc-faceted-search
 
