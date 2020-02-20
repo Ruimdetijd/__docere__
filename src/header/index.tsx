b8aee2f6b3d07dbb7dd4bacc780a6a3a8d3d8208
Gijsjan <agijsbro@gmail.com>
2020-02-20 21:00:22 +0100

    Make header less prominent

--- src/header/index.tsx
+++ src/header/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from '@emotion/styled'
-import { ASIDE_HANDLE_WIDTH, MAINHEADER_HEIGHT, TOP_OFFSET, DEFAULT_SPACING } from '../constants'
+import { MAINHEADER_HEIGHT, TOP_OFFSET, DEFAULT_SPACING, TOPMENU_HEIGHT } from '../constants'
 import PagesMenu from './pages'
 import AppContext from '../app/context'
 
@@ -8,19 +8,16 @@ const Wrapper = styled.header`
 	background: linear-gradient(to right, #988258, #c7aa71);
 	box-sizing: border-box;
 	display: grid;
-	grid-template-rows: ${ASIDE_HANDLE_WIDTH}px ${MAINHEADER_HEIGHT}px;
+	grid-template-rows: ${MAINHEADER_HEIGHT}px ${TOPMENU_HEIGHT}px;
 	height: ${TOP_OFFSET}px;
 	position: sticky;
 	top: 0;
 	z-index: 9999;
 
-    z-index: 9999;
     background-image: url(/static/images/bg.jpg);
     background-position: bottom;
-    background-color: #a1aebb;
-    background-blend-mode: color-burn;
-    border-bottom: 6px solid #082d3f;
-    background-size: 100% auto;
+	background-size: 100% auto;
+	box-shadow: 0 2px 3px black;
 `
 
 const TopMenu = styled.div`
@@ -32,35 +29,12 @@ const TopMenu = styled.div`
 	& > div {
 		display: grid;
 		grid-template-columns: 64px auto 28px;
-
-		& > input { 
-			background-color: #988258;
-			border: none;
-			box-sizing: border-box;
-			color: white;
-			font-size: 1rem;
-			grid-column: 2;
-			outline: none;
-			padding: 0 .5em;
-			width: 100%;
-		}
-		& > div {
-			align-items: center;
-			background-color: #988258;
-			grid-column: 3;
-			padding-right: 8px;
-			display: grid;
-
-			svg {
-				fill: #c7aa71;
-			}
-		}
 	}
 `
 
 const H1 = styled('h1')`
 	cursor: pointer;
-	font-size: 2em;
+	font-size: 1.25em;
 	margin: 0;
 	text-transform: uppercase;
 	align-self: center;
@@ -68,7 +42,7 @@ const H1 = styled('h1')`
     color: white;
     text-shadow: 1px 1px 5px #404040;
 	letter-spacing: .075em;
-	line-height: 96px;
+	line-height: ${TOP_OFFSET}px;
 
 	a:hover, a:link, a:active, a:visited {
 		color: inherit;
